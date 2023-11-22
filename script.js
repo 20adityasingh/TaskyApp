bdbddb40ebf01fd9b3fb4e5ddd07c8c8a47c3e15
20adityasingh <adusingh62@gmail.com>
2023-11-22 18:50:55 +0530

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("connected");
+const state ={
+    taskList: [],
+};
+
+const taskContent= document.querySelector(".task_content");
+const taskModal= document.querySelector(".task__modal_body");
+
+console.log(taskContent);
